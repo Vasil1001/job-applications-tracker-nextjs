8c79527607e9496a9044a912df475090ebdda5cb
Vasil <vaskodzhakov@gmail.com>
2023-06-25 23:35:53 +0100

    Drag and drop columns and cards into columns
    Drag and drop columns and cards into columns functionality added

--- components/Board.tsx
+++ components/Board.tsx
@@ -5,10 +5,14 @@ import { DragDropContext, DropResult, Droppable } from "react-beautiful-dnd";
 import Column from "./Column";
 
 export default function Board() {
-  const [board, getBoard] = useBoardStore((state) => [
-    state.board,
-    state.getBoard,
-  ]);
+  const [board, getBoard, setBoardState, updateJobInDB] = useBoardStore(
+    (state) => [
+      state.board,
+      state.getBoard,
+      state.setBoardState,
+      state.updateJobInDB,
+    ]
+  );
 
   // const getJobsList = useBoardStore((state) => state.getJobsList);
 
@@ -16,9 +20,80 @@ export default function Board() {
     getBoard();
   }, [getBoard]);
 
-  const handleOnDragEnd = (result: DropResult) => {};
-  // This step is needed
-  
+  const handleOnDragEnd = (result: DropResult) => {
+    const { destination, source, type } = result;
+    // check if card dropped outside the board
+    if (!destination) return;
+
+    // Handle a column drag
+    if (type === "column") {
+      const entries = Array.from(board.columns.entries());
+      const [removed] = entries.splice(source.index, 1);
+      entries.splice(destination.index, 0, removed);
+
+      const rearrangedColumns = new Map(entries);
+      setBoardState({
+        ...board,
+        columns: rearrangedColumns,
+      });
+    }
+
+    // Handle a job card drag
+    const columns = Array.from(board.columns);
+    const startColIndex = columns[Number(source.droppableId)];
+    const finishColIndex = columns[Number(destination.droppableId)];
+
+    const startCol: Column = {
+      id: startColIndex[0],
+      jobs: startColIndex[1].jobs,
+    };
+    const finishCol: Column = {
+      id: finishColIndex[0],
+      jobs: finishColIndex[1].jobs,
+    };
+
+    if (!startCol || !finishCol) return; // if no destination or start
+
+    if (source.index === destination.index && startCol === finishCol) return;
+
+    const newJobs = startCol.jobs;
+    const [jobMoved] = newJobs.splice(source.index, 1);
+
+    // ? IF Drag in the same column
+    if (startCol.id === finishCol.id) {
+      newJobs.splice(destination.index, 0, jobMoved);
+      const newCol = {
+        id: startCol.id,
+        jobs: newJobs,
+      };
+      const newColumns = new Map(board.columns);
+      newColumns.set(startCol.id, newCol);
+      setBoardState({ ...board, columns: newColumns });
+    } else {
+      // ? IF  Drag to a different column
+      const finishJobs = Array.from(finishCol.jobs);
+      finishJobs.splice(destination.index, 0, jobMoved);
+
+      const newColumns = new Map(board.columns);
+      const newCol = {
+        id: startCol.id,
+        jobs: newJobs,
+      };
+      console.log(startCol);
+      console.log(finishCol);
+
+      newColumns.set(startCol.id, newCol);
+      newColumns.set(finishCol.id, {
+        id: finishCol.id,
+        jobs: finishJobs,
+      });
+
+      // Update in DB
+      updateJobInDB(jobMoved, finishCol.id);
+      setBoardState({ ...board, columns: newColumns });
+    }
+  };
+
   return (
     <DragDropContext onDragEnd={handleOnDragEnd}>
       <Droppable droppableId="board" direction="horizontal" type="column">
@@ -28,11 +103,9 @@ export default function Board() {
             {...provided.droppableProps}
             ref={provided.innerRef}
           >
-            { 
-            Array.from(board.columns).map(([id, column], index) => (
+            {Array.from(board.columns).map(([id, column], index) => (
               <Column key={id} id={id} jobs={column.jobs} index={index} />
-            ))
-            }
+            ))}
           </div>
         )}
       </Droppable>
--- store/BoardStore.ts
+++ store/BoardStore.ts
@@ -1,11 +1,14 @@
+import { databases } from "@/appwrite";
 import { getAllJobsList } from "@/lib/getAllJobsList";
 import { getJobsGroupedByColumn } from "@/lib/getJobsGroupedByColumn";
-import { Board, Column, TypedColumn } from "@/typings";
+import { Board, Column, TypedColumn, Job } from "@/typings";
 import { create } from "zustand";
 
 interface BoardState {
   board: Board;
   getBoard: () => void;
+  setBoardState: (board: Board) => void;
+  updateJobInDB: (job: Job, columnId: TypedColumn) => void;
   // getJobsList: () => void;
 }
 
@@ -18,7 +21,19 @@ export const useBoardStore = create<BoardState>((set, get) => ({
     const board = await getJobsGroupedByColumn();
     set({ board });
   },
-  setBoardState: (board: Board) => set({ board }),
+  setBoardState: (board) => set({ board }),
+
+  updateJobInDB: async (job, columnId) => {
+    await databases.updateDocument(
+      process.env.NEXT_PUBLIC_DATABASE_ID!,
+      process.env.NEXT_PUBLIC_JOBS_COLLECTION_ID!,
+      job.$id,
+      {
+        title: job.title,
+        status: columnId,
+      }
+    );
+  },
   // getJobsList: async () => {
   //   const board = await getAllJobsList();
   //   set({ board });
